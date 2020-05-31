9de56a410e16fcc20c903c2a29e45d55bbae7175
Kseniya <zlaya_kobra@bk.ru>
2020-05-31 19:55:30 +0300

    Added ConditionerTest

--- src/main/java/ru/netology/domain/Conditioner.java
+++ src/main/java/ru/netology/domain/Conditioner.java
@@ -1,4 +1,50 @@
 package ru.netology.domain;
 
 public class Conditioner {
+    private String name;
+    private int maxTemperature;
+    private int minTemperature;
+    private int currentTemperature;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMaxTemperature() {
+        return maxTemperature;
+    }
+
+    public void setMaxTemperature(int maxTemperature) {
+        this.maxTemperature = maxTemperature;
+    }
+
+    public int getMinTemperature() {
+        return minTemperature;
+    }
+
+    public void setMinTemperature(int minTemperature) {
+        this.minTemperature = minTemperature;
+    }
+
+    public int getCurrentTemperature() {
+        return currentTemperature;
+    }
+
+    public void setCurrentTemperature(int currentTemperature) {
+        this.currentTemperature = currentTemperature;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    private boolean on;
 }
---
+++ src/test/java/ru/netology/domain/ConditionerTest.java
@@ -0,0 +1,15 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConditionerTest {
+    @Test
+    public void increaseCurrentTemperature() {
+        Conditioner conditioner = new Conditioner();
+
+    }
+
+
+}
