4bdc25b8b6aa34d44b3455821c868b90afa4231e
Kseniya <zlaya_kobra@bk.ru>
2020-06-13 12:44:47 +0300

    Conditioner amendment

--- src/main/java/ru/netology/domain/Conditioner.java
+++ src/main/java/ru/netology/domain/Conditioner.java
@@ -44,6 +44,12 @@ public class Conditioner {
     }
 
     public void setCurrentTemperature(int currentTemperature) {
+        if (currentTemperature > maxTemperature) {
+            return;
+        }
+        if (currentTemperature < minTemperature) {
+            return;
+        }
 
         this.currentTemperature = currentTemperature;
     }
